59775f1189318d6f27854b3a16e6f6670a89d07e
preeti23 <lively_aqua@yahoo.com>
2015-09-27 16:34:48 -0700

    First commit

---
+++ testngMaven/src/test/java/com/testng/maven/testngMavenTest.java
@@ -0,0 +1,11 @@
+package com.testng.maven;
+
+import org.testng.annotations.Test;
+
+public class testngMavenTest {
+ 
+	@Test
+  public void testMavenExample() {
+	  System.out.println("This is TestNG-Maven Example");
+  }
+}
